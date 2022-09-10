5327acc76b0280108b7048976474cc9413496e33
Guido Perre <perreguido@gmail.com>
2022-09-10 19:47:41 -0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
     </div>
   );
+}
+
+function Header() {
+    return (
+        <header className="Header">
+            <div className="Logo_Container">
+                <img className="Logo_Image"
+                     src={process.env.PUBLIC_URL + '/logo/jopit_logo.png'}
+                     alt="logo" />
+                <p className="Logo_Name">jopit</p>
+            </div>
+
+            <div className="Page_Navigation_Container">
+                <div className="Navigation">
+                    <p className="Page_Style">¿Quienes somos?</p>
+                    <p className="Page_Style">Precio</p>
+                    <p className="Page_Style">FAQ</p>
+                    <p className="Page_Style">Contacto</p>
+                </div>
+                <div className="Google_Login">
+                    <img className="Google_Image"
+                         src={process.env.PUBLIC_URL + '/social/google_icon.png'}
+                         alt="logo" />
+                </div>
+                <div className="Facebook_Login">
+                    <img className="Facebook_Image"
+                         src={process.env.PUBLIC_URL + '/social/facebook_icon.png'}
+                         alt="logo" />
+                </div>
+                <div className="Normal_Login">
+                    <p className="Normal_Login_Text">INGRESAR</p>
+                </div>
+            </div>
+
+        </header>
+    );
 }
 
 export default App;
