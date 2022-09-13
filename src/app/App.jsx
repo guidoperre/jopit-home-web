73f6c626320ed4ac204b2293333e1367921bc188
Guido Perre <perreguido@gmail.com>
2022-09-13 19:34:43 -0300

    price

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,6 +5,7 @@ import {Home} from "../home/Home";
 import {Description} from "../description/Description";
 import {Header} from "../header/Header";
 import {Footer} from "../footer/Footer";
+import {Price} from "../price/Price";
 
 export default function App() {
     return (
@@ -12,6 +13,7 @@ export default function App() {
             <Header/>
             <Home/>
             <Description/>
+            <Price/>
             <Footer/>
             <ToastContainer/>
         </div>
